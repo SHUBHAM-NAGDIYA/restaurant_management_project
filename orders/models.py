984a7c6d40e6015f40912f3bcdb2856bc540d574
Shubham Nagdiya <@shubhamnagdiya277@gmail.com>
2026-03-21 22:28:14 +0530

    MenuCategory Model created

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    
+class Order(models.Model):
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
