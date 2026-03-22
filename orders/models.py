78b1555c3c59ea2c7dbfb81cf5cb4054435cc1e5
Shubham Nagdiya <@shubhamnagdiya277@gmail.com>
2026-03-22 19:19:24 +0530

    Order model updated

--- orders/models.py
+++ orders/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Order models here.
 
+
 # OrderStaus
 class OrderStatus(models.Model):
     name = models.CharField(max_length=100,unique=True)
 
+# I had made Order Table  at the time of creating order status
 # Orders    
 class Order(models.Model):
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, on_delete = models.SET_NULL, null = True)
