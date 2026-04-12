62e1eef54353e4a1500519930f2cee8ab2cd5101
Shubham Nagdiya <@shubhamnagdiya277@gmail.com>
2026-04-12 14:04:45 +0530

    Restaurant

--- home/models.py
+++ home/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    
+    is_featured = models.BooleanField(default=False)
+
+class Restaurant(models.Model):
+    has_delivery = models.BooleanField(default=False)
+    pass
